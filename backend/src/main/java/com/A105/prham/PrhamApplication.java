356d05a0c920df13750093eb810921db1e2003de
rt8632 <rt8632@naver.com>
2025-10-28 17:44:00 +0900

    [S13P31A105-15] feat : mongoDB 세팅

--- backend/src/main/java/com/A105/prham/PrhamApplication.java
+++ backend/src/main/java/com/A105/prham/PrhamApplication.java
@@ -2,8 +2,10 @@ package com.A105.prham;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class PrhamApplication {
 
 	public static void main(String[] args) {
